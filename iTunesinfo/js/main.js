6c44a36b38513a585004ca145169d6d394bc4bdf
dommura@gmail.com <dommura@gmail.com>
2017-09-17 22:09:41 +0200

    First Commit

--- iTunesinfo/js/main.js
+++ iTunesinfo/js/main.js
@@ -7,5 +7,34 @@ $(document).ready(() => {
 });
 
 function getMusic(searchText){
-   axios.get('https://itunes.apple.com/search?parameterkeyvalue');
+   const COINDESK_URI = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+
+function receiveData(data) {
+  const conversionData = data.bpi
+  const coinData = document.getElementById('coin-data')
+
+  for (const currency in conversionData) {
+    const node = document.createElement('li')
+    node.innerText = 'One bitcoin buys you ' + conversionData[currency].code + ' ' + conversionData[currency].rate
+    coinData.append(node)
+  }
+}
+
+function grabData() {
+  const xhr = new XMLHttpRequest()
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) return
+    console.log(JSON.parse(xhr.response))
+  }
+
+  xhr.open('GET', COINDESK_URI)
+  xhr.send()
+}
+
+function init() {
+  grabData()
+}
+
+init()
 }
